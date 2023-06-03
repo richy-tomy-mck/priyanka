b82a7a0a7253ac2b0a99c3dee9ad3f48b655c61b
Richy Tomy <Richy_Tomy@mckinsey.com>
2023-06-03 13:10:48 -0700

    oidc issuer

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,23 +4,6 @@ locals {
   fqdn                      = var.domain_name == null ? "" : var.domain_name
   default_request_template  = file("../api/lambda.default.request.vtl")
   default_response_template = file("../api/lambda.default.response.vtl")
-  content_securty_policy    = "default-src 'self' 'unsafe-inline' 'unsafe-eval'; connect-src 'self' https://auth.int.mckinsey.id ${module.course_api.appsync_url} https://${aws_s3_bucket.background_assets.bucket}.s3.amazonaws.com; img-src 'self' https://ddls-cdn.s3.amazonaws.com/ https://www.mckinsey.com/ http://www.w3.org/2000/svg; font-src 'self' https://cdn.mckinsey.com/; media-src 'self' https://${aws_s3_bucket.background_assets.bucket}.s3.amazonaws.com;"
-  curator_request_template  = "${path.root}/../api/lambda.contributor.request.vtl"
-  headers_request_template  = "${path.root}/../api/lambda.default.withheader.request.vtl"
   enable_logging            = var.env == "dev" || var.env == "stage" || var.env == "prod" ? true : false
 }
 
-locals {
-  videoAssetsTable          = module.dynamodb_tables.tables["videoAssets"]
-  videoAssetsKey            = module.dynamodb_tables.keys["videoAssets"]
-  producedVideoAssetsTable  = module.dynamodb_tables.tables["producedVideoAssets"]
-  producedVideoAssetsKey    = module.dynamodb_tables.keys["producedVideoAssets"]
-  coursesTable              = module.dynamodb_tables.tables["courses"]
-  coursesKey                = module.dynamodb_tables.keys["courses"]
-  phoneticTable             = module.dynamodb_tables.tables["phonetic"]
-  phoneticKey               = module.dynamodb_tables.keys["phonetic"]
-  knowledgeAssessmentsTable = module.dynamodb_tables.tables["knowledgeAssessments"]
-  knowledgeAssessmentsKey   = module.dynamodb_tables.keys["knowledgeAssessments"]
-  userCourseMetricsTable    = module.dynamodb_tables.tables["userCourseMetrics"]
-  userCourseMetricsKey      = module.dynamodb_tables.keys["userCourseMetrics"]
-}
